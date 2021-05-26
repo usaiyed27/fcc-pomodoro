e276ffb979c50e916f712749d01e4d432353a7ab
Urusa <usaiyed.9127@gmail.com>
2021-05-25 19:34:45 -0600

    initial files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PomodoroSession from './components/pomodoroSession.js';
+
+function App() {
+  return (
+    <div className="App">
+      <PomodoroSession />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PomodoroSession.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const PomodoroSession = () => {
+	return(
+		<div id='session-label'>
+		 <h1>Pomodoro Session</h1>
+		</div>
+	)
+}
+
+export default PomodoroSession;
